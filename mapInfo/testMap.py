beb1a4579002dc9f8c9ec8de8e48dc180b2bea20
tristan <tristanplouz@gmail.com>
2020-12-11 16:28:06 +0100

    cc

--- mapInfo/testMap.py
+++ mapInfo/testMap.py
@@ -3,26 +3,29 @@ import websockets
 import time
 import dronekit
 
-print("Connection au slave....")
-slave = dronekit.connect("/dev/ttyUSB0",baud=56700)
-time.sleep(3)
-print("Slave: "+str(slave.version))
+def connect()
+    print("Connection au slave....")
+    slave = dronekit.connect("/dev/ttyUSB0",baud=56700)
+    time.sleep(3)
+    print("Slave: "+str(slave.version))
 
-print("Connection au master....")
-master = dronekit.connect("/dev/ttyACM0",baud=56700)
-time.sleep(5)
-print("Master: "+str(master.version))
+    print("Connection au master....")
+    master = dronekit.connect("/dev/ttyACM0",baud=56700)
+    time.sleep(5)
+    print("Master: "+str(master.version))
+    return slave,master
 
 async def processus(websocket, path):
     message = await websocket.recv()
     print(message)
-    latM,lonM,headM =master.location.global_frame.lat,master.location.global_frame.lon,master.heading
+    latM,lonM,headM = master.location.global_frame.lat,master.location.global_frame.lon,master.heading
     latS,lonS,headS = slave.location.global_frame.lat,slave.location.global_frame.lon,slave.heading
     while True:
         await websocket.send('{"type":"masterP","lat":'+str(latM)+',"lon":'+str(lonM)+',"head":'+str(headM)+'}')
         await websocket.send('{"type":"slaveS","lat":'+str(latS)+',"lon":'+str(lonS)+',"head":'+str(headS)+'}')
         time.sleep(5)
 
+slave,master = connect()
 start_server = websockets.serve(processus, "localhost", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
