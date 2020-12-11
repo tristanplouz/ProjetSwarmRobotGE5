5bc9652f9b1d98b1e3a5f240a725970b18e1b9f1
tristan <tristanplouz@gmail.com>
2020-12-11 10:27:56 +0100

    Ajout de l'automatique

--- 1slave.py
+++ 1slave.py
@@ -1,9 +1,24 @@
 import dronekit
 import time
 import sys
+import atexit
 from math import *
 
-def getSlavePos(lat_org,lon_org,d=2,teta=180):
+
+def failsafe():
+    "Methode permettant de mettre en securité les buggys"
+    slave.groundspeed = 0
+    master.groundspeed = 0
+    slave.mode = dronekit.VehicleMode("HOLD")
+    master.mode = dronekit.VehicleMode("HOLD")
+    slave.disarm()
+    master.disarm()
+    slave.close()
+    master.close()
+    
+
+def destinationPoint(lat_org,lon_org,d=2,teta=180):
+    "Methode permettant de calculer une position gps a partir d'une position GPS d'une distance et d'un cap"
     R=6371009
     teta*=pi/180
     lat_org*=pi/180
@@ -14,6 +29,48 @@ def getSlavePos(lat_org,lon_org,d=2,teta=180):
     lon=((lon+540)%360-180)*180/pi
     print("targeted: "+str(lat)+"  "+str(lon))
     return dronekit.LocationGlobal(lat,lon,0)
+    
+def haversineDistance(lat1,lon1,lat2,lon2):
+    "Haversine formula to calculate a distance"
+    R=6371009
+    lat1 *= pi/180
+    lon1 *= pi/180
+    lat2 *= pi/180
+    lon2 *= pi/180
+    
+    dlon = lon2-lon1
+    dlat = lat2-lat1
+    
+    a = sin(dlat/2)**2+cos(lat1)*cos(lat2)*(sin(dlon/2))**2
+    d = 2 * R *asin(sqrt(a))
+    
+    theta = atan2(cos(lat2)*sin(dlon),cos(lat1)*sin(lat2)-sin(lat1)*cos(lat2)*cos(dlon)
+    theta *= 180/pi
+    return d
+    
+def controller(dact,dobj,vmax):
+    "Loopback system to control the system"
+    epsilon = 0.1
+    delta = dact-dobj
+    
+    if(abs(delta)<epsilon):
+        beta = 0
+    elif(epsilon<=abs(delta) and abs(delta)<2*epsilon):
+        beta = (abs(delta)-epsilon)/epsilon
+    else:
+        beta = 1
+    
+    if(delta>0):
+        vlim = vmax*dact/abs(dact)
+    elif(delta = 0)
+        vlim=0
+    else:
+        vlim = -vmax*dact/abs(dact)
+    
+    return beta*vlim
+    
+    
+atexit.register(failsafe)
 
 print("Connection au slave....")
 slave = dronekit.connect("/dev/ttyUSB0",baud=56700)
@@ -41,16 +98,14 @@ if(slave.gps_0.fix_type>2):
     slave.mode = dronekit.VehicleMode("HOLD")
 else:
     print("No 3D fix")
-    slave.disarm()
-    master.disarm()
-    slave.close()
-    master.close()
     sys.exit()
 
 d=0.5
 theta=180
-ts=1
-gs=2
+ts=0.5
+vmax=2
+vslave = 0 
+veloc = 0
 
 while master.gps_0.satellites_visible < 17 or slave.gps_0.satellites_visible < 17 :
     print("Number of satellites unsatisfied : Master " 
@@ -60,20 +115,36 @@ while master.gps_0.satellites_visible < 17 or slave.gps_0.satellites_visible < 1
 
 input("Good satellites. Let's go ?")
 while 1:
-    lat=master.location.global_frame.lat
-    lon=master.location.global_frame.lon
+    latM=master.location.global_frame.lat
+    lonM=master.location.global_frame.lon
+    
+    latS = slave.location.global_frame.lat
+    lonS = slave.location.global_frame.lon
+    
+    dact = haversineDistance(latM,lonM,latS,latS)
+    
+    vslaveL = vslave 
+    
+    vslave = controller(dact,d,vmax)
+    
+    slavePos = ts*(vslaveL+vslave)/2
+    
     bearing = master.heading
     vx,vy,vz = master.velocity
+    velocL = veloc
     veloc = sqrt(vx**2+vy**2+vz**2)
+    
+    masterPos = ts*(velocL+veloc)/2
+    
     print("Velocity : " + str(veloc))
     if veloc > 1 :
         first_stop = False
         slave.mode = dronekit.VehicleMode("GUIDED")
-        slave.simple_goto(getSlavePos(lat,lon,d,theta+bearing),groundspeed=gs)
+        slave.simple_goto(destinationPoint(lat,lon,masterPos-slavePos,theta+bearing),groundspeed=vmax)
     else :
         if !first_stop :
             first_stop = True
-            slave.simple_goto(getSlavePos(lat,lon,d,theta+bearing),groundspeed=gs)
+            slave.simple_goto(destinationPoint(lat,lon,masterPos-slavePos,theta+bearing),groundspeed=vmax)
         else :
             slave.mode = dronekit.VehicleMode("HOLD")
     time.sleep(ts)
